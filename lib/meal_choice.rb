6c13aa98054c00e337a9909f4618c5bbe04d5214
Yehong Piao <piaoyehong1107@gmail.com>
2020-06-23 05:14:37 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,5 +1,5 @@
 def meal_choice(veg1, veg2, protein = "tofu")
-  if meal_choice(carrots, string beans, and lentils)
+  if meal_choice(carrots, string beans,lentils)
     puts "A plate of #{protein} with #{veg1} and #{veg2}."
   else puts "What a nutritious meal!"
 end
